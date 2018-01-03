770f2c3962b0899057a06bf168ebb79a0950a7b1
Luigi Filippo Pavan <luigifilippo.pavan@gmail.com>
2018-01-03 23:28:18 +0100

    added password hashing and success/error pages

--- src/main/java/it/disi/unitn/buybuy/auth/Signup.java
+++ src/main/java/it/disi/unitn/buybuy/auth/Signup.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.disi.unitn.buybuy.auth;
 
 import it.unitn.aa1617.webprogramming.persistence.utils.dao.exceptions.DAOException;
@@ -10,23 +5,22 @@ import it.unitn.aa1617.webprogramming.persistence.utils.dao.exceptions.DAOFactor
 import it.unitn.aa1617.webprogramming.persistence.utils.dao.factories.DAOFactory;
 import it.unitn.disi.buybuy.dao.UserDAO;
 import it.unitn.disi.buybuy.dao.entities.User;
-import it.unitn.disi.buybuy.dao.jdbc.JDBCUserDAO;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
 
-/**
- *
- * @author maxfrax
- */
 public class Signup extends HttpServlet {
 
     private UserDAO userDao;
+    private SecureRandom random;
 
     @Override
     public void init() throws ServletException {
@@ -39,49 +33,99 @@ public class Signup extends HttpServlet {
         } catch (DAOFactoryException ex) {
             throw new ServletException("Impossible to get dao factory for user storage system", ex);
         }
+        // Initialize RNG
+        this.random = new SecureRandom();
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        request.getRequestDispatcher("signup.html").forward(request, response);
+        req.getRequestDispatcher("signup.html").forward(req, res);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        
+        /* TODO
+            - email validation using JavaMail
+            - username validation using regex
+            - add setSalt() to User DAO (add column to DB schema)
+        */
+        
+        // Parameters validation
         boolean validInput = true;
-        // Fields validation
         String email = req.getParameter("email");
-        validInput = validInput && email.contains("@");
-        // Config of User object 
-        if (validInput) {
-            User user = new User();
-            user.setEmail(email);
-            user.setName(req.getParameter("name"));
-            user.setLastname(req.getParameter("surname"));
-            user.setType(User.Type.REGISTRATION_PENDING);
-            user.setUsername(req.getParameter("username"));
-            // TODO encrypt password
-            user.setHashPassword(req.getParameter("pass"));
-            // Check if insert gone okay            
-            try {
-                userDao.insert(user);
-                resp.sendRedirect("/BuyBuy");
-            } catch (DAOException ex) {
-                Logger.getLogger(Signup.class.getName()).log(Level.SEVERE, null, ex);
-                // TODO parametro errore
-                doGet(req, resp);
-            }
+        validInput = email.contains("@");
+        if (!validInput) {
+            res.sendRedirect(req.getContextPath() + "/error.jsp");
         }
+
+        // Generate password hash
+        byte[] salt = getSalt();
+        byte[] password = req.getParameter("pass").getBytes();
+        byte[] bytes = prependSalt(salt, password);
+        String hashPassword = null;
+        try {
+            hashPassword = sha256(bytes);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Signup.class.getName()).log(Level.SEVERE, null, ex);
+            res.sendRedirect(req.getContextPath() + "/error.jsp");
+        }
+        
+        // Create user
+        User user = new User();
+        user.setEmail(email);
+        user.setName(req.getParameter("name"));
+        user.setLastname(req.getParameter("surname"));
+        user.setType(User.Type.REGISTRATION_PENDING);
+        user.setUsername(req.getParameter("username"));
+        user.setHashPassword(hashPassword);
+        
+        // Insert user into DB
+        try {
+            userDao.insert(user);
+            res.sendRedirect(req.getContextPath() + "/success.jsp");
+        } catch (DAOException ex) {
+            Logger.getLogger(Signup.class.getName()).log(Level.SEVERE, null, ex);
+            res.sendRedirect(req.getContextPath() + "/error.jsp");
+        }
+
+    }
+
+    /**
+     * Generate random 32-byte length salt to use with password hash.
+     * @return an array of byte representing salt
+     */
+    private byte[] getSalt() {
+        byte[] salt = new byte[32];
+        this.random.nextBytes(salt);
+        return salt;
+    }
+
+    /**
+     * Prepend salt to password.
+     * @param salt salt to prepend
+     * @param password password
+     * @return Password with salt prepended
+     */
+    private byte[] prependSalt(byte[] salt, byte[] password) {
+        byte[] result = new byte[salt.length + password.length];
+        System.arraycopy(salt, 0, result, 0, salt.length);
+        System.arraycopy(password, 0, result, salt.length, password.length);
+        return result;
+    }
+
+    /**
+     * Generate SHA-256 string from an array of bytes.
+     * @param bytes input array of the SHA-256 function
+     * @return hash string calculated from input array
+     * @throws NoSuchAlgorithmException 
+     */
+    private String sha256(byte[] bytes) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(bytes);
+        String hash = DatatypeConverter.printHexBinary(md.digest());
+        return hash;
     }
 
 }
