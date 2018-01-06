5622e1c905f15b6b8ecd53eaac8ee93d6433f98a
Luigi Filippo Pavan <luigifilippo.pavan@gmail.com>
2018-01-06 12:24:29 +0100

    changed error messages

--- src/main/java/it/unitn/disi/buybuy/auth/Signup.java
+++ src/main/java/it/unitn/disi/buybuy/auth/Signup.java
@@ -56,29 +56,29 @@ public class Signup extends HttpServlet {
         // Name
         String name = req.getParameter("name").trim();
         if (name == null || name.isEmpty()) {
-            errors.add("Inserisci il tuo nome.");
+            errors.add("Inserisci il nome");
         }
 
         // Lastname
         String lastname = req.getParameter("lastname").trim();
         if (lastname == null || lastname.isEmpty()) {
-            errors.add("Inserisci il tuo cognome.");
+            errors.add("Inserisci il cognome");
         }
 
         // Username
         String username = req.getParameter("username").trim();
         if (username == null || username.isEmpty()) {
-            errors.add("Inserisci uno username.");
+            errors.add("Inserisci uno username");
         } else if (!isValidUsername(username)) {
-            errors.add("Lo username può essere lungo tra i 3 e i 10 caratteri e può contenere solo lettere, numeri, e i simboli . - _.");
+            errors.add("Lo username può essere lungo tra i 3 e i 10 caratteri e può contenere solo lettere, numeri, e i simboli . - _");
         } else {
             try {
                 if (isDuplicateUsername(username)) {
-                    errors.add("Lo username inserito è già in uso.");
+                    errors.add("Lo username inserito è già in uso");
                 }
             } catch (DAOException ex) {
                 errors.clear();
-                errors.add("Errore interno. Riprovare più tardi.");
+                errors.add("Errore interno, riprovare più tardi");
                 req.getRequestDispatcher("signup.jsp").forward(req, res);
                 return; // Stop executing this servlet.
             }
@@ -87,17 +87,17 @@ public class Signup extends HttpServlet {
         // Email
         String email = req.getParameter("email").trim();
         if (email == null || email.isEmpty()) {
-            errors.add("Inserisci il tuo indirizzo email.");
+            errors.add("Inserisci l'indirizzo email");
         } else if (!isValidEmail(email)) {
-            errors.add("Indirizzo email non corretto.");
+            errors.add("Indirizzo email non corretto");
         } else {
             try {
                 if (isDuplicateEmail(email)) {
-                    errors.add("L'indirizzo email inserito è già in uso.");
+                    errors.add("L'indirizzo email inserito è già in uso");
                 }
             } catch (DAOException ex) {
                 errors.clear();
-                errors.add("Errore interno. Riprovare più tardi.");
+                errors.add("Errore interno, riprovare più tardi");
                 req.getRequestDispatcher("signup.jsp").forward(req, res);
                 return; // Stop executing this servlet.
             }
@@ -106,13 +106,13 @@ public class Signup extends HttpServlet {
         // Password
         String password = req.getParameter("password");
         if (password == null || password.isEmpty()) {
-            errors.add("Inserisci una password.");
+            errors.add("Inserisci una password");
         }
 
         // Privacy agreement (checkbox)
         String privacy = req.getParameter("privacy");
         if (privacy == null) {
-            errors.add("L'accettazione della normativa sulla privacy è obbligatoria.");
+            errors.add("L'accettazione della normativa sulla privacy è obbligatoria");
         }
 
         // If there are errors, forward to JSP with same request
@@ -141,7 +141,7 @@ public class Signup extends HttpServlet {
             userDao.insert(user);
             req.getRequestDispatcher("success.jsp").forward(req, res);
         } catch (NoSuchAlgorithmException | DAOException ex) {
-            errors.add("Errore interno. Riprovare più tardi.");
+            errors.add("Errore interno, riprovare più tardi");
             req.getRequestDispatcher("signup.jsp").forward(req, res);
         } 
 
