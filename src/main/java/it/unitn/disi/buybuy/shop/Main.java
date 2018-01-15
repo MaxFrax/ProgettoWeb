21414d2fcfd412483288d6f26889bc321ab2007b
MaxFrax <maxfrax@gmail.com>
2018-01-15 10:41:18 +0100

    List of category to index.jsp

--- src/main/java/it/unitn/disi/buybuy/shop/Main.java
+++ src/main/java/it/unitn/disi/buybuy/shop/Main.java
@@ -5,8 +5,15 @@
  */
 package it.unitn.disi.buybuy.shop;
 
+import it.unitn.aa1617.webprogramming.persistence.utils.dao.exceptions.DAOException;
+import it.unitn.aa1617.webprogramming.persistence.utils.dao.exceptions.DAOFactoryException;
+import it.unitn.aa1617.webprogramming.persistence.utils.dao.factories.DAOFactory;
+import it.unitn.disi.buybuy.dao.CategoryDAO;
+import it.unitn.disi.buybuy.dao.entities.Category;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +24,21 @@ import javax.servlet.http.HttpServletResponse;
  * @author maxfrax
  */
 public class Main extends HttpServlet {
+    
+    private CategoryDAO categoryDAO;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            DAOFactory daoFactory = (DAOFactory) super.getServletContext().getAttribute("daoFactory");
+            if (daoFactory == null) {
+                throw new DAOFactoryException("Failed to get DAO factory.");
+            }
+            categoryDAO = daoFactory.getDAO(CategoryDAO.class);
+        } catch (DAOFactoryException ex) {
+            throw new ServletException(ex.getMessage(), ex);
+        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -30,7 +52,18 @@ public class Main extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setAttribute("categories", retrieveCategories());
         request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+    
+    private List<Category> retrieveCategories() {
+        List<Category> output = null;
+        try {
+            output = categoryDAO.getAll();
+        } catch (DAOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return output;
     }
 
 }
